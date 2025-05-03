692da09d0e84d187d0185c6dc8890e1bd60518d2
tanveerprottoy <tanveer.prottoy@gmail.com>
2025-05-03 15:04:42 +0600

    fix main function

--- cmd/main.go
+++ cmd/main.go
@@ -3,9 +3,6 @@ package main
 import "log"
 
 func main() {
-	// this will block pushing to the repo by the pre push hook
-	return
-
 	log.Println("Hello, World!")
 	log.Println("This is a simple Go program for Github actions.")
 }
