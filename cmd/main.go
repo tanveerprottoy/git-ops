c68776f597a6a1d429d78ce0cbefb882f3713a9f
tanveerprottoy <tanveer.prottoy@gmail.com>
2025-05-03 15:10:59 +0600

    add linter env

--- cmd/main.go
+++ cmd/main.go
@@ -4,5 +4,5 @@ import "log"
 
 func main() {
 	log.Println("Hello, World!")
-	log.Println("This is a simple Go program for Github actions.")
+	log.Println("This is a simple Go program for Github actions")
 }
